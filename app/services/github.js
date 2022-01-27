58434b03d8de87e18c5d01b809848403cda50fa1
Ben Everard <ben@theideabureau.co>
2022-01-27 14:42:13 +0000

    Adjusted Github authorisation to use headers

--- app/services/github.js
+++ app/services/github.js
@@ -3,22 +3,10 @@ var GitHubApi = require('github');
 var config = require('../config');
 
 var github = new GitHubApi({
-    // required
-    version: '3.0.0'
-    // // optional
-    // debug: true,
-    // protocol: "https",
-    // host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub
-    // pathPrefix: "/api/v3", // for some GHEs; none for GitHub
-    // timeout: 5000,
-    // headers: {
-    //     "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-    // }
-});
-
-github.authenticate({
-    type: 'oauth',
-    token: config.ghToken
+    version: '3.0.0',
+    headers: {
+        Authorization: config.ghToken
+    }
 });
 
 module.exports.connection = github;
